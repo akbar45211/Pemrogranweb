5d7f9843eb1f0417f5487ea12dbde3e7cdd1eaba
akbar45211 <115201909+akbar45211@users.noreply.github.com>
2022-10-07 09:56:53 +0700

    Add files via upload

---
+++ Pertemuan1/content.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<div class="row my-1 ">
+                <div class="col-xl col-6 d-flex align-items-center justify-content-center my-2" style="height: 500px;">
+                    <div class="card" style="width: 15rem;">
+                        <img src="png/item 1.png" height="200px">
+                        <div class="card-body">
+                           <h5 class="card-title">Card title</h5>
+                           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                           <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl col-6 d-flex align-items-center justify-content-center my-2" style="height: 500px;">
+                    <div class="card" style="width: 15rem;">
+                        <img src="png/item 2.png" height="200px">
+                        <div class="card-body">
+                           <h5 class="card-title">Card title</h5>
+                           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                           <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl col-6 d-flex align-items-center justify-content-center my-2" style="height: 500px;">
+                    <div class="card" style="width: 15rem;">
+                        <img src="png/item 3.png" height="200px">
+                        <div class="card-body">
+                           <h5 class="card-title">Card title</h5>
+                           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                           <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl col-6 d-flex align-items-center justify-content-center my-2" style="height: 500px;">
+                    <div class="card" style="width: 15rem;">
+                        <img src="png/item 4.png" height="200px">
+                        <div class="card-body">
+                           <h5 class="card-title">Card title</h5>
+                           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                           <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row my-1 ">
+                <div class="col-xl col-6 d-flex align-items-center justify-content-center my-2" style="height: 500px;">
+                    <div class="card" style="width: 15rem;">
+                        <img src="png/item 5.png" height="200px">
+                        <div class="card-body">
+                           <h5 class="card-title">Card title</h5>
+                           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                           <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl col-6 d-flex align-items-center justify-content-center my-2" style="height: 500px;">
+                    <div class="card" style="width: 15rem;">
+                        <img src="png/item 6.png" height="200px">
+                        <div class="card-body">
+                           <h5 class="card-title">Card title</h5>
+                           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                           <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl col-6 d-flex align-items-center justify-content-center my-2" style="height: 500px;">
+                    <div class="card" style="width: 15rem;">
+                        <img src="png/item 7.png" height="200px">
+                        <div class="card-body">
+                           <h5 class="card-title">Card title</h5>
+                           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                           <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl col-6 d-flex align-items-center justify-content-center my-2" style="height: 500px;">
+                    <div class="card" style="width: 15rem;">
+                        <img src="png/item 8.png" height="200px">
+                        <div class="card-body">
+                           <h5 class="card-title">Card title</h5>
+                           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                           <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+</html>
